eb0d4197e9e696daf36b7e7748c88c73175f25d9
Colin Gillespie <ratchetandcolin@gmail.com>
2020-02-24 00:46:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Portfolio
+
+My current portfolio showcasing recent projects.
+
+[Colin Gillespie | Portfolio](https://colingillespie.xyz/)
+
+### Installing and Running
+
+Within root:
+
+```
+npm install
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style/style.css'
+        }),
+        new CopyWebpackPlugin([{ from: 'src/assets/img/', to: 'assets/img/' }])
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(gif|png|svg|gif|jpe?g)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/img/',
+                            publicPath: 'assests/img/'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
